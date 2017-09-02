7455b4f491ebb6f1760e206bdf7703ce04386d79
Andre Luis <tr.andreluis@gmail.com>
2017-09-02 00:06:35 -0300

    Criação do prototipo da tela de cadastro de Título (aplicação do bootstrap)

---
+++ cobranca/src/main/java/com/trandreluis/cobranca/controller/TituloController.java
@@ -0,0 +1,14 @@
+package com.trandreluis.cobranca.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TituloController {
+
+	@RequestMapping("/titulos/novo")
+	public String novo() {
+		return "CadastroTitulo";
+	}
+	
+}
