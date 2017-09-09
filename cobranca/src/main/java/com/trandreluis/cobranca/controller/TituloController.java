a352b39511bf50d8b26c78ea2cf4a45954d7a6ea
Andre Luis <tr.andreluis@gmail.com>
2017-09-09 16:34:52 -0300

    Submissão de dados para o servidor

--- cobranca/src/main/java/com/trandreluis/cobranca/controller/TituloController.java
+++ cobranca/src/main/java/com/trandreluis/cobranca/controller/TituloController.java
@@ -2,12 +2,25 @@ package com.trandreluis.cobranca.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.trandreluis.cobranca.model.Titulo;
 
 @Controller
+@RequestMapping("/titulos")
 public class TituloController {
 
-	@RequestMapping("/titulos/novo")
+	@RequestMapping("/novo")
 	public String novo() {
+		return "CadastroTitulo";
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public String salvar(Titulo titulo) {
+		// TODO: salvar no banco de dados
+		
+		System.out.println(">>>> "+titulo.getDescricao());
+		
 		return "CadastroTitulo";
 	}
 	
---
+++ cobranca/src/main/java/com/trandreluis/cobranca/enums/StatusTitulo.java
@@ -0,0 +1,18 @@
+package com.trandreluis.cobranca.enums;
+
+public enum StatusTitulo {
+
+	PENDENTE("Pendente"),
+	RECEBIDO("Recebido");
+	
+	private String descricao;
+	
+	StatusTitulo(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	public String getDescricao() {
+		return descricao;
+	}
+	
+}
