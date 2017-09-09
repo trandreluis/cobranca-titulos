80a512cc8c99a4d7f2273ecce57603c9ea60c233
Andre Luis <tr.andreluis@gmail.com>
2017-09-09 19:57:20 -0300

    Configuração para utilização do MySQL

--- cobranca/src/main/java/com/trandreluis/cobranca/controller/TituloController.java
+++ cobranca/src/main/java/com/trandreluis/cobranca/controller/TituloController.java
@@ -1,12 +1,17 @@
 package com.trandreluis.cobranca.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.trandreluis.cobranca.enums.StatusTitulo;
 import com.trandreluis.cobranca.model.Titulo;
 import com.trandreluis.cobranca.repository.Titulos;
 
@@ -18,8 +23,9 @@ public class TituloController {
 	private Titulos titulos;
 	
 	@RequestMapping("/novo")
-	public String novo() {
-		return "CadastroTitulo";
+	public ModelAndView novo() {
+		ModelAndView mav = new ModelAndView("CadastroTitulo");
+		return mav;
 	}
 	
 	@RequestMapping(method = RequestMethod.POST)
@@ -28,6 +34,11 @@ public class TituloController {
 		ModelAndView mav = new ModelAndView("CadastroTitulo");
 		mav.addObject("mensagem", "Título salvo com sucesso!");
 		return mav;
+	}
+
+	@ModelAttribute("todosStatusTitulo")
+	public List<StatusTitulo> todosStatusTitulo() {
+		return Arrays.asList(StatusTitulo.values());
 	}
 	
 }
