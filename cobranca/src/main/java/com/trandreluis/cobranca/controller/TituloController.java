95266984019687ac925b112f739ef1b4e7174a6b
Andre Luis <tr.andreluis@gmail.com>
2017-09-12 22:41:50 -0300

    Mantendo valores no formulário em caso de erro de validação

--- cobranca/src/main/java/com/trandreluis/cobranca/controller/TituloController.java
+++ cobranca/src/main/java/com/trandreluis/cobranca/controller/TituloController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.trandreluis.cobranca.enums.StatusTitulo;
 import com.trandreluis.cobranca.model.Titulo;
@@ -40,14 +41,13 @@ public class TituloController {
 	}
 
 	@RequestMapping(method = RequestMethod.POST)
-	public ModelAndView salvar(@Validated Titulo titulo, Errors errors) {
-		ModelAndView mav = new ModelAndView("CadastroTitulo");
+	public String salvar(@Validated Titulo titulo, Errors errors, RedirectAttributes attributes) {
 		if(errors.hasErrors()) {
-			return mav;
+			return "CadastroTitulo";
 		}
 		titulos.save(titulo);
-		mav.addObject("mensagem", "Título salvo com sucesso!");
-		return mav;
+		attributes.addFlashAttribute("mensagem", "Título salvo com sucesso!");
+		return "redirect:/titulos/novo";
 	}
 
 	@ModelAttribute("todosStatusTitulo")
